c71c669644693bb51b50089b19721b59ca5d6b3d
Susan Pommer <kspommer@gmail.com>
2018-09-02 07:35:07 -0700

    Trying to execute simple data pass from App.js to Books.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,53 @@
 // import React libraru
-import React from 'react'
+import React, {Component} from 'react'
 // import API
 import * as BooksAPI from './BooksAPI'
 // import style sheet
 import './App.css'
 // import components
-//import Books from './Books'
+import Books from './Books'
+
+const books = [
+  {
+  //"imageURL": "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")", 
+  "title": "To Kill a Mockingbird",
+  "authors": "Harper Lee", 
+  "state": "Currently Reading"
+  },
+  { 
+  //"imageURL": "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api", 
+  "title": "Ender\'s Game", 
+  "authors": "Orson Scott Card", 
+  "state": "Currently Reading"
+  }
+]  
 
 class BooksApp extends React.Component {
-  state = {
+  //state = {
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-   books: []
+   //books: []
     // deleted line here in original App.js
-  }
+  //}
 
   // make call to API  
-  componentDidMount(){
-    BooksAPI.getAll().then(book => {
-        this.setState({books: books})
-    })
-  }
+  //componentDidMount(){
+    //BooksAPI.getAll().then(book => {
+        //this.setState({books: books})
+   // })
+  //}
 
-  // trying to print the book array returned by API 
   render() {
     return (
-        console.log(books)
+        <div>
+          // Display books 
+          // Pass over books array
+          <Books books={books}/>
+        </div>  
     )
   }
 }
