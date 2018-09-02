0724f908ca5d597828a969c50932373bda6bd5f5
Susan Pommer <kspommer@gmail.com>
2018-09-02 07:35:43 -0700

    Trying to receive Books data from App.js (API non-op in App.JS still)

--- src/Books.js
+++ src/Books.js
@@ -1,17 +1,7 @@
+// Display book sub-image to page
+
 // import React 
 import React, {Component} from 'react'
-
-// project pseudocode draft
-// on start, fetchall()
-// add state from last render (where stored?)
-// display based on last render 
-// if state != CurrentlyReading or WantToRead, assume State = Read
-
-// if user hits +, then nav to search page
-// if user searches,  the books array
-// add current state for any books in the last array 
-
-// if user changes state (arrow picklist) then render with new state
 
 // Book component
 class Books extends Component {
@@ -20,17 +10,22 @@ class Books extends Component {
 	// need to add book-changer div / component 
 	// <div key={book.id}, className="book-shelf-changer">ADD BOOK SHELF CHANGER COMPONENT HERE</div>	
 	render() {
+		console.log('Props, this.props') // TEST
 		return ( 
 			<div className='book'>
+			// map over each book and display certain fields
 				{books.map (book => (
-					<div key={book.title} className='book-top'>CHANGE THIS</div>,
-						<div key={book.title} className='book-cover' style={width: 128 height: 188 background-image='url{book.image}'}></div>
-					<div key={book.title} className='book-title'>{book.title}</div,	
-					<div key={book.title} className='book-authors'>{book.authors}</div,				
-				)}
+					<div key={book.title} className='book-title'>{book.title}</div>,
+					<div key={book.title} className='book-authors'>{book.authors}</div>				
+				))}
 			</div>	
 		)
 	}			
 }
 
-export default BookList
+export default Books
+
+
+
+//<div key={book.title} className='book-top'>CHANGE THIS</div>,
+//	<div key={book.title} className='book-cover' style={width: 128 height: 188 background-image='url{book.image}'}></div>
