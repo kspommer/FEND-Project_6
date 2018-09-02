d7752fc0c0a928a32211f8cee16243cb8774cbb8
Susan Pommer <kspommer@gmail.com>
2018-09-02 14:51:31 -0700

    Converted Books.js to functional component

--- src/Books.js
+++ src/Books.js
@@ -3,36 +3,33 @@
 // import React 
 import React, {Component} from 'react'
 
-// Book component
-class BookList extends Component {
-	// render a book
+// Book component converted to stateless function 
+function BookList(props) {
 	// map over each book and display certain fields (cover image, title, author)
-	render() {
-		return (
- 			<ol className="books-grid">
-				{this.props.books.map(book => (
-					<li>
-						<div className='book'>
-							<div className="book-top">
-								<div key={book.id} className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>							
-			                    <div className="book-shelf-changer">
-			                      <select>
-			                        <option value="move" disabled>Move to...</option>
-			                        <option value="currentlyReading">Currently Reading</option>
-			                        <option value="wantToRead">Want to Read</option>
-			                        <option value="read">Read</option>
-			                        <option value="none">None</option>
-			                      </select>
-			                    </div>
-			            	</div>
-			            	<div key={book.id} className="book-title">{book.title}</div>
-			            	<div key={book.id} className="book-authors">{book.authors}</div>	
-						</div>
-					</li>	
-				))}
-			</ol>        
-		)
-	}			
+	return (
+		<ol className="books-grid">
+			{props.books.map(book => (
+				<li>
+					<div className='book'>
+						<div className="book-top">
+							<div key={book.id} className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>							
+		                    <div className="book-shelf-changer">
+		                      <select>
+		                        <option value="move" disabled>Move to...</option>
+		                        <option value="currentlyReading">Currently Reading</option>
+		                        <option value="wantToRead">Want to Read</option>
+		                        <option value="read">Read</option>
+		                        <option value="none">None</option>
+		                      </select>
+		                    </div>
+		            	</div>
+		            	<div key={book.id} className="book-title">{book.title}</div>
+		            	<div key={book.id} className="book-authors">{book.authors}</div>	
+					</div>
+				</li>	
+			))}
+		</ol>        
+	)			
 }
       
 export default BookList
