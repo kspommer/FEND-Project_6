56c07107e6526917fb88375f7ad8ae5a67e5b624
Susan Pommer <kspommer@gmail.com>
2018-09-02 11:24:04 -0700

    Books displaying in UI (all on Currently Reading Shelf) / images and bookshelfchanger need additional work

--- src/Books.js
+++ src/Books.js
@@ -5,25 +5,34 @@ import React, {Component} from 'react'
 
 // Book component
 class BookList extends Component {
-
-	// render a book (cover image, title, author)
-	// need to add book-changer div / component 
-	// <div key={book.id}, className="book-shelf-changer">ADD BOOK SHELF CHANGER COMPONENT HERE</div>	
-	// map over each book and display certain fields
+	// render a book
+	// map over each book and display certain fields (cover image, title, author)
 	render() {
-		return ( 
-			<div className='book'>
+		return (
+ 			<ol className="books-grid">
 				{this.props.books.map(book => (
-					<li key={book.title} className='book-title'>{book.title}</li> 		
+					<li>
+						<div className='book'>
+							<div className="book-top">
+								<div key={book.id} className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("URL")' }}></div>,							
+			                    <div className="book-shelf-changer">
+			                      <select>
+			                        <option value="move" disabled>Move to...</option>
+			                        <option value="currentlyReading">Currently Reading</option>
+			                        <option value="wantToRead">Want to Read</option>
+			                        <option value="read">Read</option>
+			                        <option value="none">None</option>
+			                      </select>
+			                    </div>
+			            	</div>
+			            	<div key={book.id} className="book-title">{book.title}</div>
+			            	<div key={book.id} className="book-authors">{book.authors}</div>	
+						</div>
+					</li>	
 				))}
-			</div>	
+			</ol>        
 		)
 	}			
 }
-
+      
 export default BookList
-
-
-
-//<div key={book.title} className='book-top'>CHANGE THIS</div>,
-//	<div key={book.title} className='book-cover' style={width: 128 height: 188 background-image='url{book.image}'}></div>
