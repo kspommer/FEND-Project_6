1a5ead85424a4a43b1708e7695465da06bf27caa
Susan Pommer <kspommer@gmail.com>
2018-09-03 17:19:54 -0700

    STUCK ON ERRORS (src/Book.js and src/Shelf.js)

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,36 +1,67 @@
 // Display three shelves
+// Makes call to Book component to render each book on its specific shelf
 
 // import React 
 import React, {Component} from 'react'
 // import components
-import BookList from './Books'
+import Book from './Books'
+import App from './App'
 
 // Shelf component
 class Shelves extends Component {
+  // filter by shelf to create three unique book arrays
+  arrayCurrentlyReading = this.state.books.filter(book => (book.readingModes.shelf === "currentlyReading"))
+  arrayWantToRead = this.state.books.filter(book => (book.readingModes.shelf === "wantToRead"))
+  arrayRead = this.state.books.filter(book => (book.readingModes.shelf === "read"))
 
-  getShelf(book) {
-    let shelf = ''
-
-    (this.book.readingModes.shelf === 'currentlyReading' ? shelf="Currently Reading" : 
-      (this.book.readingModes.shelf === 'wantToRead' ? shelf="Want to Read" : 
-        (this.book.readingModes.shelf === 'read' ? shelf="Read" : null)))
-  }
-
-// map over each book and figure out what shelf they are on
+  // map over each book and figure out what shelf they are on
   render() {
-    return (
-      <div className="need to add here"> 
-        {this.props.books.map(book => (
-          <div key={book.id} className="bookshelf"> 
-            <h2 className="bookshelf-title">getShelf(book)</h2>
-            <div className="bookshelf-books">
-              <BookList/>
-            </div>   
+    return ( 
+      <div>
+        // filter the list of books three times and render list items
+        {arrayCurrentlyReading.map((book) => (        
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <li>
+                    <Book book={this.state.book}/> 
+                  </li>
+                </ol>      
+              </div>
+          </div>  
+        ))}  
+
+        {arrayWantToRead.map((book) => (        
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <li>
+                    <Book book={this.state.book}/> 
+                  </li>
+                </ol>      
+              </div>
           </div>
         ))} 
-      </div>     
-    )
-  }      
-}
+
+        {arrayRead.map((book) => (        
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <li>
+                    <Book book={this.state.book}/> 
+                  </li>
+                </ol>      
+              </div>
+          </div>  
+        ))}
+      </div>  
+    )  
+  }
+}        
 
 export default Shelves
+
+{/*Filter reference:  https://stackoverflow.com/questions/42894774/react-filter-method-without-mutating-state*/}
