486caef1d7e406f66aa8ddc956a38c109de1be79
Susan Pommer <kspommer@gmail.com>
2018-09-08 12:36:05 -0700

    Saturday work-in-progress getting book / shelf components working

--- src/Books.js
+++ src/Books.js
@@ -2,36 +2,44 @@
 
 // import React 
 import React, {Component} from 'react'
-// import components
-import Shelves from './Shelf'
-import App from './App'
 
 // Book component  - displays thumbnail image, title, authors(NEED WORK), picklist
-class Book extends Component {
+class Books extends Component {
 	// map over each book and display certain fields (cover image, title, author)
+ 	state = {
+ 		title: this.props.title, 
+ 		authors: this.props.authors,
+ 		imageLink: this.props.imageLink,
+ 		coverImage: this.props.coverImage,
+ 	};
+
 	render() {
+		console.log('props', this.props) // TESTING 
+		
 		return (
-			<div className="book">
-				<div className="book-top">
-					<div key={book.id} className="book-cover" style={{ width: 128, height: 193,
-						backgroundImage: book.imageLinks && book.imageLinks.thumbnail ?
-		      			`url(${book.imageLinks.thumbnail})`:
-		      			"url('./img/not-available.jpg')"}}></div>						
-		            <div className="book-shelf-changer">
-		              <select>
-		                <option value="move" disabled>Move to...</option>
-		                <option value="currentlyReading">Currently Reading</option>
-		                <option value="wantToRead">Want to Read</option>
-		                <option value="read">Read</option>
-		                <option value="none">None</option>
-		              </select>
-		            </div>
-		    	</div>
-		    	<div key={book.id} className="book-title">{book.title}</div>
-		    	<div key={book.id} className="book-authors">{book.authors}</div>	
-			</div>
+			this.state.books.map((book) => ( 
+				<div className="book">
+					<div className="book-top">
+						<div key={book.title} className="book-cover" style={{ width: 128, height: 193,
+							backgroundImage: book.imageLink && book.coverImage ?
+			      			`url(${book.coverImage})`:
+			      			"url('./img/not-available.jpg')"}}></div>						
+			            <div className="book-shelf-changer">
+			              <select>
+			                <option value="move" disabled>Move to...</option>
+			                <option value="currentlyReading">Currently Reading</option>
+			                <option value="wantToRead">Want to Read</option>
+			                <option value="read">Read</option>
+			                <option value="none">None</option>
+			              </select>
+			            </div>
+			    	</div>
+			    	<div key={book.id} className="book-title">{book.title}</div>
+			    	<div key={book.id} className="book-authors">{book.authors}</div>	
+				</div>
+			))
 		)
 	}
 }     				
       
-export default Book
+export default Books
