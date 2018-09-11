f893bbfa699f3d493b84291deaa9d32eab8e90c5
Susan Pommer <kspommer@gmail.com>
2018-09-11 08:18:46 -0700

    Added + (add book) images to Shelf page

--- src/Shelf.js
+++ src/Shelf.js
@@ -29,21 +29,29 @@ class Shelves extends Component {
   // map over each book for that shelf to display required books
   render() { 
     return ( 
-      <div>          
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {this.props.books && this.props.books.map(book => ( 
-                  <li>
-                    <Books key={book.title} title={book.title} authors={book.authors} 
-                    coverImage={book.imageLinks.thumbnail} imageLinks={book.imageLinks}/>
-                  </li>
-                ))}  
-              </ol>      
-            </div>
-        </div>   
-      </div>
+      <div>
+        <div>          
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {this.props.books && this.props.books.map(book => ( 
+                    <li>
+                      <Books key={book.title} title={book.title} authors={book.authors} 
+                      coverImage={book.imageLinks.thumbnail} imageLinks={book.imageLinks}/>
+                    </li>
+                  ))}  
+                </ol>      
+              </div>
+          </div>   
+        </div>
+    
+        <div>
+          <div className="open-search">
+            <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          </div>
+        </div>
+      </div>  
     )
   }
 }    
