88ad5493a602ed3891ebeaeba4ce2eb1ffbca98b
Susan Pommer <kspommer@gmail.com>
2018-09-14 17:33:34 -0700

    First attempt to refactor the shelfChanger to change book.shelf value

--- src/App.js
+++ src/App.js
@@ -29,20 +29,44 @@ class BooksApp extends Component {
 
   // method to drive shelf changes (from picklist)
   shelfChanger = (book, shelf) => {
-    // call API to update the book's shelf (returns an updated book object)
-    BooksAPI.update(book, shelf) 
+    // record the current state
+    const currentState = this.state
+    console.log(currentState) // TESTING - WORKING 
+    console.log(book.id) // TESTING - WORKING 
+    const oldShelf = book.shelf 
+    console.log(oldShelf) // TESTING - WORKING
+
+    // remove that book.id from the books array?   
+
+    // call API to update the book's shelf (returns a promise)
+    BooksAPI.update(book, shelf).then(response) { // returns a promise
+      // using that book.id's, get newShelf from promise - HOW???
+      // set book.shelf = newShelf
+      
+      // add updatedBook back to books array -- use push(book)? 
+
+      // get all books again?  
+
+      //reset the book state
+      //this.setState({books})
+
     // call API to get updated books and reset state
     BooksAPI.getAll().then((books => {
       this.setState({books})
+      console.log(book.shelf) // TESTING 
     }))
-  }
+  } // TESTING - WORKING 
 
 // Display books; pass books array to Shelf component 
 // Shelf components calls Book component to display book
   render() {
     return (
-      <div className="app">
-        {this.state.showSearchPage ? (
+      <div className="app, want to find the book.id and read new shelf
+      // set book.shelf">
+        {this.state.showSearchPage
+
+
+         ? (
           <div className="search-books">
             <div className="search-books-bar">
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
